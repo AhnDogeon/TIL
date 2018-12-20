6a31e02c94f4a58d0b50533a459847303d07e1e5
Ahndogeon <qltiqlti@gmail.com>
2018-12-20 15:41:41 +0900

    181220

--- 0_startcamp/day1/am_i_lucky.py
+++ 0_startcamp/day1/am_i_lucky.py
@@ -46,32 +46,51 @@ print(my_numbers)
 #####################
 
 # my_numbers, real_numbers, bonus_number
-
-for number in my_numbers:
-    cor_number = len(set(number)&set(self.real_numbers[:6]))
-# 1 등 : 
-if cor_number == 6:
-                print('\t{:2} {:2} {:2} {:2} {:2} {:2}'.format(*i[:6]),end=' >>> ')
-                print('1등 당첨')
-                self.rank[0] += 1
-# 2 등 : real & my 가 5개가 같고, my의 나머지 하나가 bonus_number
-elif cor_number == 5 and bonus_number == True
-                print('2등 당첨')
-# 3 등 : real & my 가 5개가 같다.
-elif cor_number == 5:
-                print('\t{:2} {:2} {:2} {:2} {:2} {:2}'.format(*i[:6]),end=' >>> ')
-                print('3등 당첨')
-                self.rank[0] += 1
-# 4 등 : real & my 가 4개가 같다.
-elif cor_number == 4:
-                print('\t{:2} {:2} {:2} {:2} {:2} {:2}'.format(*i[:6]),end=' >>> ')
-                print('4등 당첨')
-                self.rank[0] += 1
-# 5 등 : real & my 가 3개가 같다.
-elif cor_number == 3:
-                print('\t{:2} {:2} {:2} {:2} {:2} {:2}'.format(*i[:6]),end=' >>> ')
-                print('5등 당첨')
-                self.rank[0] += 1
-# 꽝
+### 내 답
+same_numbers = list(set(my_numbers).intersection(real_numbers))
+if len(same_numbers) == 6:
+        print("1등")
+elif len(same_numbers) == 5:
+        if bonus_number in my_numbers and len(same_numbers) == 5:
+                print("2등")
+        elif len(same_numbers) == 5:
+                print("3등")
+elif len(same_numbers) == 4:
+        print("4등")
+elif len(same_numbers) == 3:
+        print("5등")
 else:
-        print('꽝')
+        print("꽝")
+
+
+### 선생님 답
+
+## 방법 1. for문을 이용
+count = 0
+for my_numbers in my_numbers:
+        for real_number in real_numbers:
+                if my_number == real_number:
+                        count += 1
+if count == 6:
+        print(1)
+elif count == 5 and bonus in my_numbers:
+        print(2)
+elif count == 5:
+        print(3)
+ 
+## 방법 2. 집합을 이용! 집합은 set 자료구조 set은 리스트처럼 인덱스로 순서가 있는게 아님
+## list = [1,2,3] 이렇게 순서로 구분
+## tuple = (1,2,3)
+
+## set = {1,2,3} 딕셔너리랑 구분해야한다, 인덱스로 접근 불가
+
+my_numbers = set([1,2,3,4,5,6])
+real_numbers = set([1,2,3,4,5,6])
+bonus = 7
+
+match_count = len(my_numbers & real_numbrs)
+
+if match_count == 6:
+        print('1등')
+elif match_count == 5 and bonus in my_numbers:
+        print('2등')
