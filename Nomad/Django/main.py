6a54df0d24e95fb4da07b0d5ed83df7c581ab874
AhnDogeon <qltiqlti@gmail.com>
2021-02-03 22:07:03 +0900

    210203 | class method

--- Nomad/Django/main.py
+++ Nomad/Django/main.py
@@ -12,8 +12,17 @@ print(plus(3, 4, 1,1,1,1,1,1,1,1,hello=True, dasf='sda', vvv=False))
 class Car():
     wheels = 4
     door = 4
-    windows = 4
-    seats = 4
+    # 이건 function, class 안에 있으면 method
+    def start(self):
+        print(self.color)
+        print("I started")
+
+    def __init__(self, *args, **kwargs):
+        self.windows = 4
+        self.seats = 4
+        self.color = kwargs.get("color", "black")
+        self.price = kwargs.get("price", "0")
+
 
 # kia와 hyundai instance 생성
 # intro of OOP
@@ -22,3 +31,11 @@ kia.color = 'white'
 
 hyundai = Car()
 hyundai.color = 'black'
+
+porche = Car("GREEN", "$40")
+# 이대로 실행한다면 argument error가 나게 된다. 
+# 파이썬에서 method의 첫번째 인자로 self를 주는데 method에는 인자가 0개라서!
+# 따라서 위  method에서 self인자를 넣어줄 것
+# -> 여기서 self는 instance 자신이 된다! (self.color에서 Super Red가 출력되는 것)
+porche.color = "Super Red"
+porche.start()
